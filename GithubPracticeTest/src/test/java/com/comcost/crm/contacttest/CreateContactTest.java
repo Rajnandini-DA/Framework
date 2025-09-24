237588470a4df3efdecb7da05a5e9e82166de1a3
abkmo <abkmo@LAPTOP-4RN0ANDJ>
2025-09-24 13:14:59 +0530

    Sopln

--- GithubPracticeTest/src/test/java/com/comcost/crm/contacttest/CreateContactTest.java
+++ GithubPracticeTest/src/test/java/com/comcost/crm/contacttest/CreateContactTest.java
@@ -74,7 +74,7 @@ public class CreateContactTest {
 			System.out.println(lastname + "----failed");
 		}
 		
-		System.out.println("Header verified");
+		System.out.println("Header done");
 
 		// Logout of Application
 		WebElement Logout = driver.findElement(By.xpath("//img [@src='themes/softed/images/user.PNG']"));
